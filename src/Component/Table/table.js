04e5e4dd30bb5d3f3a6a2aa2de16b7e9aba459af
AkshatDaxini <akshatdaxini1@gmail.com>
2022-12-04 12:21:12 -0500

    first commit

---
+++ src/Component/Table/table.js
@@ -0,0 +1,32 @@
+import './table.css'
+import avatar from "../../assets/images/img_avatar.png";
+import avatar2 from "../../assets/images/img_avatar2.png";
+const Card = () => {
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card-content">
+                    <img src={avatar} alt="Profile image" /><br />
+                    <div className="text-zone">
+                        <span>Akshat Daxini</span>
+                        <p>Some random things about yourself </p>
+                    </div>
+                    <a href="https://akshatdaxiniportfolio.netlify.app/"><button className="btn-portfolio">Portfolio</button></a>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="card-content">
+                    <img src={avatar2} alt="Profile image" /><br />
+                    <div className="text-zone">
+                        <span>Arpi Kothari</span>
+                        <p>Some random things about yourself </p>
+                    </div>
+                    <a href="https://akshatdaxiniportfolio.netlify.app/"><button className="btn-portfolio">Portfolio</button></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
