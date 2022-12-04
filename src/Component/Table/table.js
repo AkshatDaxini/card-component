0752f32b5417018112fe29f63b3aa5211bea8468
AkshatDaxini <akshatdaxini1@gmail.com>
2022-12-04 12:43:44 -0500

    Minore changes

--- src/Component/Table/table.js
+++ src/Component/Table/table.js
@@ -19,7 +19,7 @@ const Card = () => {
                 <div className="card-content">
                     <img src={avatar2} alt="Profile" /><br />
                     <div className="text-zone">
-                        <span>Arpi Kothari</span>
+                        <span>Divya</span>
                         <p>Some random things about yourself </p>
                     </div>
                     <a href="https://akshatdaxiniportfolio.netlify.app/"><button className="btn-portfolio">Portfolio</button></a>
